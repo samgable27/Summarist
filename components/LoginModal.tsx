4689eaf4691259dd334c8bfcd57c995272bffce0
Sam <samarooniii@yahoo.com>
2023-04-03 22:12:05 -0400

    modal styling almost done

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Button, Modal, Form, Input, ConfigProvider } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 import { useModalStore } from "../pages/store";
-
+import styles from "../modal.module.css";
 type Props = {};
 
 const LoginModal = (props: Props) => {
@@ -10,42 +10,64 @@ const LoginModal = (props: Props) => {
   const visible = useModalStore((state) => state.visible);
   const closeModal = useModalStore((state) => state.closeModal);
 
-  const handleSubmit = (values: { username: string; password: string }) => {
+  const handleSubmit = (values: { email: string; password: string }) => {
     console.log("Login values:", values);
-    // Perform login logic here
     closeModal();
   };
 
+  const customTitle = (
+    <div className={styles.customTitle}>Log in to Summarist</div>
+  );
+
   return (
-    <Modal
-      title="Login"
-      open={visible}
-      onCancel={closeModal}
-      footer={null}
-      destroyOnClose
-    >
-      <Form form={form} onFinish={handleSubmit} layout="vertical">
-        <Form.Item
-          name="username"
-          label="Username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
-    </Modal>
+    <div className={styles.modal__background}>
+      <Modal
+        title={customTitle}
+        open={visible}
+        onCancel={closeModal}
+        footer={null}
+        destroyOnClose={true}
+        wrapClassName={styles.modal}
+      >
+        <Form form={form} onFinish={handleSubmit} layout="vertical">
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email address!" },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined />}
+              className={styles.input__field}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              className={styles.inputField__password}
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ width: "100%", backgroundColor: "#2bd97c" }}
+              className={styles.login__btn}
+            >
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
   );
 };
 

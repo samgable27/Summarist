3c9c342c4b4cf38d48a92a4176c26e32c9863f12
Sam <samarooniii@yahoo.com>
2023-04-26 13:11:50 -0400

    added library html

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import libStyles from "../styles/library.module.css";
 
 interface LibraryProps {
   children?: React.ReactNode;
@@ -6,8 +7,23 @@ interface LibraryProps {
 
 const Library: React.FC<LibraryProps> = () => {
   return (
-    <div>
-      <h1>Library</h1>
+    <div className={libStyles.libContainer}>
+      <div>
+        <span>Saved Books</span>
+        <p>0 items</p>
+      </div>
+      <div className={libStyles.libBlockWrapper}>
+        <h2>Save your favorite books!</h2>
+        <p>When you save a book, it will appear here.</p>
+      </div>
+      <div>
+        <span>Finished</span>
+        <p>0 items</p>
+        <div className={libStyles.libBlockWrapper}>
+          <h2>Done and dusted!</h2>
+          <p>When you finish a book, you can find it here later.</p>
+        </div>
+      </div>
     </div>
   );
 };

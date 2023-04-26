1ba5584659b1e8bceea1a379a6f3755ac737d5fb
Sam <samarooniii@yahoo.com>
2023-04-26 17:04:43 -0400

    CSS to cap max-width at 1600px for library and settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -9,15 +9,17 @@ const Settings: React.FC<SettingsProps> = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className={styles.sectionTitle}>Settings</div>
-        <div className={styles.settingContentTop}>
-          <h2>Your Subscription Plan</h2>
-          <p>Basic</p>
-          <button>Upgrade to Premium</button>
-        </div>
-        <div className={styles.settingContentBtm}>
-          <h2>Email</h2>
-          <p>youremail@gmail.com</p>
+        <div className={styles.settingWrapper}>
+          <div className={styles.sectionTitle}>Settings</div>
+          <div className={styles.settingContentTop}>
+            <h2>Your Subscription Plan</h2>
+            <p>Basic</p>
+            <button>Upgrade to Premium</button>
+          </div>
+          <div className={styles.settingContentBtm}>
+            <h2>Email</h2>
+            <p>youremail@gmail.com</p>
+          </div>
         </div>
       </div>
     </div>

f57c75ec09d57d050467a3f27363c632880c4014
Sam <samarooniii@yahoo.com>
2023-05-09 19:38:06 -0400

    removed stripe logic replace w extension

--- src/lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY!;
-
-if (!stripePublicKey) {
-  throw new Error("Stripe public key is missing in environment variables.");
-}
-
-export const stripePromise = loadStripe(stripePublicKey);
